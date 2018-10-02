d0fb13fb5e60ba81de95045a207f84304345d657
juan <juanan_rp@hotmail.co.uk>
2018-10-02 10:47:25 +0100

    creating initial file for customer identification details

---
+++ test/models/Reads/PsaSubscriptionDetails/CustomerIdentificationDetailsReadsSpec.scala
@@ -0,0 +1,8 @@
+package models.Reads.PsaSubscriptionDetails
+
+import models.Samples
+import org.scalatest.{MustMatchers, OptionValues, WordSpec}
+
+class CustomerIdentificationDetailsReadsSpec extends WordSpec with MustMatchers with OptionValues with Samples {
+
+}
