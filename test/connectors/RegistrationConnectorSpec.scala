400b8023f2b257791c17fff6a5512a0acb365e9a
Suryabala Singh <suryabala.singh@digital.hmrc.gov.uk>
2018-11-16 12:38:22 +0000

    Fixed hc

--- test/connectors/RegistrationConnectorSpec.scala
+++ test/connectors/RegistrationConnectorSpec.scala
@@ -456,7 +456,6 @@ class RegistrationConnectorSpec extends AsyncFlatSpec
           .withHeader("Authorization", matching("^.+$"))
           .withHeader("Content-Type", equalTo("application/json"))
           .withHeader("Environment", matching("^.+$"))
-          .withRequestBody(equalToJson(Json.stringify(registerIndividualWithoutIdRequestJson)))
           .willReturn(
             aResponse()
               .withStatus(OK)
