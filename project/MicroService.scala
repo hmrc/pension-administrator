dded4861ccb51592fbbaf5873032f6f618699581
Suryabala Singh <suryabala.singh@digital.hmrc.gov.uk>
2018-11-22 16:00:42 +0000

    Fixed all the compilation errors

--- project/MicroService.scala
+++ project/MicroService.scala
@@ -1,7 +1,6 @@
 import sbt.Keys._
 import sbt.Tests.{Group, SubProcess}
 import sbt._
-import play.routes.compiler.StaticRoutesGenerator
 import play.sbt.PlayImport.PlayKeys
 import play.sbt.routes.RoutesKeys
 import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin._
@@ -14,7 +13,6 @@ trait MicroService {
   import uk.gov.hmrc.{SbtBuildInfo, ShellPrompt, SbtAutoBuildPlugin}
   import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
   import uk.gov.hmrc.versioning.SbtGitVersioning
-  import play.sbt.routes.RoutesKeys.routesGenerator
   import uk.gov.hmrc.versioning.SbtGitVersioning.autoImport.majorVersion
 
   import TestPhases._
--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -29,7 +29,7 @@ object MicroServiceBuild extends Build with MicroService {
   private val scalacheckVersion = "1.14.0"
   private val domainVersion = "5.2.0"
   private val scalacheckGenRegexp = "0.1.1"
-  private val bootstrapVersion = "0.31.0"
+  private val bootstrapVersion = "0.32.0"
   private val playJsonVersion = "2.6.10"
   private val scalaTestVersion = "3.0.5"
   private val reactiveMongoVersion = "6.2.0"
