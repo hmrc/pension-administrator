ebed55b7b73af0a7e7e18eee9206af09e98dba8d
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2019-06-24 11:30:39 +0100

    PODS-2759 Some audit events

---
+++ test/audit/EmailAuditEventSpec.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package audit
+
+import models.Sent
+import models.enumeration.JourneyType
+import org.scalatest.{FlatSpec, Matchers}
+import uk.gov.hmrc.domain.PsaId
+
+class EmailAuditEventSpec extends FlatSpec with Matchers {
+
+  "EmailAuditEvent" should "output the correct map of data" in {
+
+    val event = EmailAuditEvent(
+      psaId = PsaId("A2500001"),
+      event = Sent,
+      journeyType = JourneyType.PSA
+
+    )
+
+    val expected: Map[String, String] = Map(
+      "psaId" -> "A2500001",
+      "event" -> Sent.toString
+    )
+
+    event.auditType shouldBe s"${JourneyType.PSA}EmailEvent"
+    event.details shouldBe expected
+  }
+}
