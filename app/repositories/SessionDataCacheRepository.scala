bfa6ee93843a2456c7033d9758857e635bb5b934
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2021-03-16 10:41:00 +0000

    PODS-4932 Add new mongo cache for storing admin or practitioner

---
+++ app/repositories/SessionDataCacheRepository.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package repositories
+
+import com.google.inject.Inject
+import play.api.Configuration
+import play.modules.reactivemongo.ReactiveMongoComponent
+
+import scala.concurrent.ExecutionContext
+
+class SessionDataCacheRepository @Inject()(
+                                        config: Configuration,
+                                        component: ReactiveMongoComponent
+                                      )(implicit val ec: ExecutionContext) extends ManageCacheRepository(
+  config.underlying.getString("mongodb.pension-administrator-cache.session-data.name"),
+  Some(config.underlying.getInt("mongodb.pension-administrator-cache.session-data.timeToLiveInSeconds")),
+  component,
+  "manage.json.encryption",
+  config
+)
