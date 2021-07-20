466065d2bf964be417db23a8c85f7d30f0dc4150
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2021-07-20 09:36:14 +0100

    PODS-5496 WIP

--- app/controllers/RegistrationController.scala
+++ app/controllers/RegistrationController.scala
@@ -67,19 +67,14 @@ class RegistrationController @Inject()(
       retrieveUser { user =>
         request.body.asJson match {
           case Some(jsBody) =>
-            val utr = (jsBody \ "utr").convertTo[String]
-            val org = jsBody.convertTo[Organisation]
-            val strippedUtr = stripUtr(Some("UTR"), Some(utr))
-              
-            Try((utr, org)) match {
-              case Success((utr, org)) =>
 
+            (Try(stripUtr(Some("UTR"), Some((jsBody \ "utr").convertTo[String])), jsBody.convertTo[Organisation])) match {
+              case Success((Some(utr), org)) =>
                 val orgWithInvalidCharactersRemoved = org.copy( organisationName =
                   org.organisationName.replaceAll("""[^a-zA-Z0-9- '&\/]+""", ""))
                 val registerWithIdData = mandatoryPODSData(true).as[JsObject] ++
                   Json.obj("organisation" -> Json.toJson(orgWithInvalidCharactersRemoved))
                 registerConnector.registerWithIdOrganisation(utr, user, registerWithIdData) map handleResponse
-
               case Failure(e) =>
                 logger.warn(s"Bad Request returned from frontend for Register With Id Organisation $e")
                 Future.failed(new BadRequestException(s"Bad Request returned from frontend for Register With Id Organisation $e"))
