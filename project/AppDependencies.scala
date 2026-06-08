489ff80b02deb4e34cc6610b1264f2cd7e5f533c
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2026-06-08 08:00:49 +0100

    DDCNL-12041: Dependency updates as per PR bot comment

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,13 +19,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
-  private val mongoVersion = "2.11.0"
+  private val bootstrapVersion = "10.7.0"
+  private val mongoVersion = "2.12.0"
 
   private val compile: Seq[ModuleID] = Seq(
     ws,
     "com.fasterxml.jackson.module" %% "jackson-module-scala"                 % "2.20.0",
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"                   % mongoVersion,
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30"            % bootstrapVersion,
     "com.networknt"                %  "json-schema-validator"                % "1.5.8",
     "uk.gov.hmrc"                  %% "domain-play-30"                       % "13.0.0",
